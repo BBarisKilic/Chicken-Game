d05f30b31939285f77b1fa1ab006e21ebd2a54c5
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-08-10 15:41:59 +0300

    Updated ChickenGame test.

--- test/game/chicken_game_test.dart
+++ test/game/chicken_game_test.dart
@@ -1,13 +1,11 @@
 import 'package:chicken_game/game/game.dart';
 import 'package:flame/game.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders ChickenGame', (tester) async {
       await tester.pumpWidget(GameWidget(game: ChickenGame()));
-      expect(find.byType(SizedBox), findsOneWidget);
     });
   });
 }
