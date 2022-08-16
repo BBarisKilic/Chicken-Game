40c812ef39edb9f26958bd349319a0f80eb2c1c7
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-08-16 20:58:26 +0300

    Set device orientation.

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,10 +1,14 @@
 import 'package:chicken_game/app/app.dart';
 import 'package:chicken_game/bootstrap.dart';
 import 'package:chicken_game/injector.dart' as di;
+import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Flame.device.fullScreen();
+  await Flame.device.setLandscape();
 
   await di.initializeDependencies();
 
